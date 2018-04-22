a2ca4404c3cc7ffc79237df0852623aa363a3a28
PatHock <phock@ycp.edu>
2018-04-22 14:48:39 -0400

    Apply Sobel filter and have multiple plots in one window. Massive memory leak

--- ev/vision/object_detection.py
+++ ev/vision/object_detection.py
@@ -9,12 +9,13 @@ object_detection.py
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 # img = cv2.imread('road_country.jpg', 1)
 # if img is None:
 #     raise Exception("could not load image !")
 
-cap = cv2.VideoCapture('solidWhiteRight.mp4')
+cap = cv2.VideoCapture('roadvideo.mp4')
 if not cap.isOpened():
     print "Couldn't load video :("
 
@@ -25,6 +26,8 @@ upper_white = np.array([255, 15, 255])
 # define range of color yellow in HSV
 lower_yellow = np.array([20, 130, 100])
 upper_yellow = np.array([40, 255, 255])
+
+
 
 
 while True:
@@ -38,20 +41,46 @@ while True:
         gaussian_hsv = cv2.GaussianBlur(hsv, (5, 5), 0)
 
         # Threshold the HSV image
-        maskYellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
-        maskWhite = cv2.inRange(hsv, lower_white, upper_white)
+        # maskYellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        # maskWhite = cv2.inRange(hsv, lower_white, upper_white)
 
         maskYellowGaussian = cv2.inRange(gaussian_hsv, lower_yellow, upper_yellow)
         maskWhiteGaussian = cv2.inRange(gaussian_hsv, lower_white, upper_white)
 
-        # Bitwise-AND mask and original image
-        # resWhite = cv2.bitwise_and(frame, frame, mask=maskWhite)
-        # resYellow = cv2.bitwise_and(frame, frame, mask=maskYellow)
 
-        cv2.imshow('frame', frame)
-        cv2.imshow('maskWhite', maskWhite)
-        cv2.imshow('gaussian white mask', maskWhiteGaussian)
-        cv2.imshow(' gaussian yellow mask', maskYellowGaussian)
+
+
+        # Bitwise-AND mask and original image
+
+        # resWhite = cv2.bitwise_and(frame, frame, mask=maskWhiteGaussian)
+        # resYellow = cv2.bitwise_and(frame, frame, mask=maskYellowGaussian)
+        res = cv2.bitwise_and(frame, frame, mask=maskWhiteGaussian) + cv2.bitwise_and(frame, frame, mask=maskYellowGaussian)
+
+        # cv2.imshow('frame', frame)
+        # cv2.imshow('maskWhite', maskWhite)
+        # cv2.imshow('result', res)
+        # cv2.imshow('gaussian white mask', maskWhiteGaussian)
+        # cv2.imshow(' gaussian yellow mask', maskYellowGaussian)
+
+
+        # Sobel filtering for Canny edge detection
+        sobelx = cv2.Sobel(res, cv2.CV_64F, 1, 0, ksize=5)
+        sobely = cv2.Sobel(res, cv2.CV_64F, 0, 1, ksize=5)
+
+        # Plot
+        plt.subplot(2, 2, 1), plt.imshow(res)
+        plt.title('mask result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2, 2, 2), plt.imshow(sobelx)
+        plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2, 2, 3), plt.imshow(sobely)
+        plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+        plt.subplot(2, 2, 4), plt.imshow(sobelx + sobely)
+        plt.title('Sobel X + Sobel Y'), plt.xticks([]), plt.yticks([])
+
+        plt.show()
+
+
+
 
         k = cv2.waitKey(50) & 0xFF
         if k == 27:
